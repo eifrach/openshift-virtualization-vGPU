702c10b49513742f3fa4e1798c0493f88ec3aa19
Eran Ifrach <eifrach@redhat.com>
2023-02-23 15:46:37 +0200

    fixing support for python3-9

--- roles/deploy-vgpu-manager/files/update_policy.py
+++ roles/deploy-vgpu-manager/files/update_policy.py
@@ -5,8 +5,17 @@ from json import loads, dumps
 from base64 import b64decode
 from os import path 
 from os  import environ as env
+from sys import argv
 import argparse
 
+
+def VerifyClusterAccess(KUBECONFIG: str) -> None:
+    with oc.api_server(kubeconfig_path=KUBECONFIG):
+        try:
+            oc.selector('pods').objects()
+        except:
+            print("ERROR: Openshift API is not avilable")
+            exit(1)
 
 def getPodsNamespace(KUBECONFIG: str) -> list:
     with oc.api_server(kubeconfig_path=KUBECONFIG), oc.project(getNamespace()):
@@ -51,19 +60,45 @@ def getCurrentPolicy(KUBECONFIG: str) -> dict:
             print("ERROR: Couldn't find policy")
             exit(1)
 
-def GetkubeConfig() -> str:
-    if path.exists(args.kubeconfig):
-        return args.kubeconfig
-    else:
-        print("ERROR: couldn't locate KUBECONFIG path")
-        exit(1)
-
 def getNamespace() -> str:
     if "NAMESPACE" in env:
         return env.get("NAMESPACE")
     else:
         return "nvidia-gpu-operator"
 
+def actionGetRander() -> None:
+    if args.use_current:
+        policy = getCurrentPolicy(KUBECONFIG=GetkubeConfig())
+    else:
+        policy = getNvidiaExamplePolicy(KUBECONFIG=GetkubeConfig())
+        print(patchUpdateWithPolicy(policy=policy, update=verifyUpdateFile(args.patch)))
+
+def actionSetDefault() -> None:
+    default_policy = getNvidiaExamplePolicy(KUBECONFIG=GetkubeConfig())
+    applyJsontoOpnshift(KUBECONFIG=GetkubeConfig(), file=default_policy)
+
+def actionSetRander() -> None:
+    if args.use_current:
+        policy = getCurrentPolicy(KUBECONFIG=GetkubeConfig())
+    else:
+        policy = getNvidiaExamplePolicy(KUBECONFIG=GetkubeConfig())
+    renderd_policy = patchUpdateWithPolicy(policy=policy, update=verifyUpdateFile(args.patch))
+    if applyJsontoOpnshift(KUBECONFIG=GetkubeConfig(), file=renderd_policy):
+        print(renderd_policy)
+
+def CheckPath(kubeconfig_path: str) -> bool:
+    if path.exists(kubeconfig_path):
+        return True
+    return False
+
+def ValidateKubeConfig(kubeconfig_path: str) -> None:
+    if not CheckPath(kubeconfig_path):
+        print("ERROR: couldn't locate kubeconfig")
+        exit(1)
+
+def PreFligthChecks(kubeconfig_path: str) -> None:
+    ValidateKubeConfig(kubeconfig_path)
+    VerifyClusterAccess(kubeconfig_path) 
 
 parser = argparse.ArgumentParser(description = 'update nvidia operator policy')
 subparser = parser.add_subparsers(metavar='', dest='command')
@@ -75,16 +110,16 @@ get = subparser.add_parser("get", help="Print Infomation")
 get_subparser = get.add_subparsers(metavar='', dest='get')
 
 get_default_policy = get_subparser.add_parser("default", help="Print default policy")
-get_default_policy.add_argument('-k', '--kubeconfig', default=env.get('KUBECONFIG'),help=KUBECONFIG_DESCRIPTION)
+get_default_policy.add_argument('-k', '--kubeconfig', required=True, help=KUBECONFIG_DESCRIPTION)
 
 get_rendered_policy = get_subparser.add_parser("render", help="Print rendered policy")
-get_rendered_policy.add_argument('-k', '--kubeconfig', default=env.get('KUBECONFIG'),help=KUBECONFIG_DESCRIPTION)
+get_rendered_policy.add_argument('-k', '--kubeconfig', required=True, help=KUBECONFIG_DESCRIPTION)
 get_rendered_policy.add_argument('-u','--use-current',default=False, required=False, help="Use current policy instead of default policy, default 'False'")
 get_rendered_policy.add_argument('-p','--patch',type=str, required=True, help="json encoded base64 to patch")
 
 
 get_current_policy = get_subparser.add_parser("current", help="Print configured policy")
-get_current_policy.add_argument('-k', '--kubeconfig', default=env.get('KUBECONFIG'),help=KUBECONFIG_DESCRIPTION)
+get_current_policy.add_argument('-k', '--kubeconfig',required=True, help=KUBECONFIG_DESCRIPTION)
 
 
 ### set
@@ -92,50 +127,56 @@ set = subparser.add_parser("set", help="set configuration Infomation")
 set_subparser = set.add_subparsers(metavar='', dest='set')
 
 set_default_policy = set_subparser.add_parser("default", help="apply using default policy")
-set_default_policy.add_argument('-k', '--kubeconfig', default=env.get('KUBECONFIG'),help=KUBECONFIG_DESCRIPTION)
+set_default_policy.add_argument('-k', '--kubeconfig', required=True, help=KUBECONFIG_DESCRIPTION)
 
 set_rendered_policy = set_subparser.add_parser("render", help="apply renderd policy")
-set_rendered_policy.add_argument('-k', '--kubeconfig', default=env.get('KUBECONFIG'),help=KUBECONFIG_DESCRIPTION)
+set_rendered_policy.add_argument('-k', '--kubeconfig',required=True, help=KUBECONFIG_DESCRIPTION)
 set_rendered_policy.add_argument('-u','--use-current',default=False, required=False, help="Use current policy instead of default policy, default 'False'")
 set_rendered_policy.add_argument('-p','--patch',type=str, required=True, help="json encoded base64 to patch")
 
 
 args = parser.parse_args()
 
+def check_key_exists(key: str, searchdict: dict) -> bool:
+    if key in searchdict:
+        return True
+    return False
+
+def GetActions(key: str) -> None:
+    if key ==  "default":
+        print(dumps(getNvidiaExamplePolicy(KUBECONFIG=args.kubeconfig), indent=2 ))
+    elif key == "render":
+        actionGetRander()
+    elif key == "current":
+        getCurrentPolicy(KUBECONFIG=args.kubeconfig)
+    return
+
+def SetActions(key: str) -> None:
+    if key ==  "default":
+        actionSetDefault()
+    elif key == "render":
+        actionSetRander()
+    return
+
+GetOptions = ["default", "render", "current"]
+SetOptions=  ["default", "render" ]
 
 
-if args.command == "get":
-    match args.get:
-        case "default":
-            print(dumps(getNvidiaExamplePolicy(KUBECONFIG=GetkubeConfig()), indent=2 ))
-        case "render":
-            if args.use_current:
-                policy = getCurrentPolicy(KUBECONFIG=GetkubeConfig())
-            else:
-               policy = getNvidiaExamplePolicy(KUBECONFIG=GetkubeConfig())
-            print(patchUpdateWithPolicy(policy=policy, update=verifyUpdateFile(args.patch)))
-        case "current":
-            getCurrentPolicy(KUBECONFIG=GetkubeConfig())
-        case _:
-            get.print_help()
-            exit(1)
-
-elif args.command == "set":
-    match args.set:
-        case "default":
-            default_policy = getNvidiaExamplePolicy(KUBECONFIG=GetkubeConfig())
-            applyJsontoOpnshift(KUBECONFIG=GetkubeConfig(), file=default_policy)
-        case "render":
-            if args.use_current:
-                policy = getCurrentPolicy(KUBECONFIG=GetkubeConfig())
-            else:
-               policy = getNvidiaExamplePolicy(KUBECONFIG=GetkubeConfig())
-            renderd_policy = patchUpdateWithPolicy(policy=policy, update=verifyUpdateFile(args.patch))
-            if applyJsontoOpnshift(KUBECONFIG=GetkubeConfig(), file=renderd_policy):
-                print(renderd_policy)
-        case _:    
-            set.print_help()
-            exit(1)
-else:
+if len(argv) <= 1:
     parser.print_help()
     exit(1)
+
+PreFligthChecks(args.kubeconfig)
+
+if args.command == "get":
+    if check_key_exists(key = args.get, searchdict = GetOptions):
+        GetActions(key = args.get)
+    else:
+        get.print_help()
+        exit(1)
+elif args.command == "set":
+    if check_key_exists(key = args.set, searchdict = SetOptions):
+        SetActions(key = args.set)
+    else:
+        get.print_help()
+        exit(1)
